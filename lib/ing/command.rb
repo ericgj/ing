b875faba6ae22eaceb19d8d5e3019544d878a3b5
Eric Gjertsen <ericgj72@gmail.com>
2012-09-20 07:08:28 -0400

    Ing::Command, tests

---
+++ lib/ing/command.rb
@@ -0,0 +1,125 @@
+﻿module Ing
+
+  # Classes in this namespace provide a uniform interface to different
+  # option parsers.
+  #
+  module OptionParsers
+  
+    class Trollop
+      
+      def parser
+        @parser ||= ::Trollop::Parser.new
+      end
+      
+      def parse!(args)
+        ::Trollop.with_standard_exception_handling(parser) { parser.parse(args) }
+      end
+      
+      def describe
+        s=StringIO.new
+        parser.educate_banner s
+        s.rewind; s.to_s      
+      end
+      
+      def help
+        s=StringIO.new
+        parser.educate s
+        s.rewind; s.to_s      
+      end
+      
+    end
+    
+  end
+  
+  class Command
+  
+    class << self
+      attr_writer :parser
+      def parser
+        @parser ||= ::Ing::OptionParsers::Trollop.new  
+      end
+      
+      def execute(klass, *args, &config)
+        new(klass, *args).execute(&config)
+      end
+    end
+    
+    attr_accessor :options, :command_class, :command_meth, :args
+    
+    def initialize(klass, *args)
+      self.options        = (Hash === args.last ? args.pop : {})
+      self.command_class = klass
+      self.command_meth  = extract_method!(args, command_class)
+      self.args           = args
+    end
+    
+    def classy?
+      command_class.respond_to?(:new)
+    end
+    
+    def instance
+      @instance ||= build_command
+    end
+    
+    def execute
+      yield instance if block_given?
+      classy? ? instance.send(command_meth, *args) : 
+                instance.send(command_meth, *args, options)
+    end
+
+    def describe
+      with_option_parser {|p| p.describe}
+    end
+    
+    def help
+      with_option_parser {|p| p.help}
+    end
+    
+    def with_option_parser   # :nodoc:
+      return {} unless command_class.respond_to?(:specify_options)
+      p = self.class.parser
+      command_class.specify_options(p.parser)
+      yield p
+    end
+    
+    private
+    
+    def build_command
+      parse_options!
+      classy? ? command_class.new(options) : command_class
+    end
+ 
+    def parse_options!
+      self.options.merge! parsed_options_from_args
+    end
+    
+    # memoized to avoid duplicate args processing
+    def parsed_options_from_args
+      @parsed_options ||= with_option_parser do |p|
+                            p.parse! self.args
+                          end
+    end
+        
+    def extract_method!(args, klass)
+      return :call if args.empty?
+      if meth = whitelist(args.first, klass)
+        args.shift
+      else
+        meth = :call
+      end
+      meth
+    end
+        
+    # Note this currently does no filtering, but basically checks for respond_to
+    def whitelist(meth, klass)
+      finder = Proc.new {|m| m == meth.to_sym}
+      if klass.respond_to?(:new)
+        klass.public_instance_methods(true).find(&finder)
+      else
+        klass.public_methods.find(&finder)
+      end
+    end
+    
+  end
+  
+end
---
+++ test/unit/test_command.rb
@@ -0,0 +1,333 @@
+﻿require File.expand_path('../test_helper', File.dirname(__FILE__))
+require File.expand_path('../../lib/ing/command', File.dirname(__FILE__))
+
+describe Ing::Command do
+
+  def dummy_command_class
+    Class.new do
+      attr_reader :options
+      def initialize(options); @options = options; end
+      def dummy; end
+    end
+  end
+
+  def dummy_command_class_with_specify_options
+    Class.new do
+      def self.specify_options(expect); end
+      attr_reader :options
+      def initialize(options); @options = options; end
+      def dummy; end
+    end
+  end
+  
+  def dummy_command_proc
+    x = Proc.new { }
+    def x.dummy; end
+    x
+  end
+    
+  #-----------------------------------------------------------------------------
+  describe ".new" do
+  
+    describe "when only command class passed" do
+      subject { Ing::Command.new dummy_command_class }
+      
+      it "should have command_meth == :call" do
+        assert_equal :call, subject.command_meth
+      end
+      
+      it "should have options == {}" do
+        assert_equal Hash.new, subject.options
+      end
+      
+      it "should have args == []" do
+        assert_equal [], subject.args
+      end  
+    end
+    
+    describe "when command class and options hash passed" do
+      subject       { Ing::Command.new dummy_command_class, options }
+      let(:options) { {:one => 1, :two => 2} }
+      
+      it "should have command_meth == :call" do
+        assert_equal :call, subject.command_meth
+      end
+      
+      it "should have options == passed options" do
+        assert_equal options, subject.options
+      end
+      
+      it "should have args == []" do
+        assert_equal [], subject.args
+      end   
+    end
+    
+    describe "when command class and method passed" do
+      subject    { Ing::Command.new dummy_command_class, meth, *args }
+      let(:meth) { "dummy" }
+      let(:args) { ["one", "two"] }
+      
+      it "should have command_meth == passed method" do
+        assert_equal meth.to_sym, subject.command_meth
+      end
+      
+      it "should have options == {}" do
+        assert_equal Hash.new, subject.options
+      end
+      
+      it "should have args == remaining args" do
+        assert_equal args, subject.args
+      end   
+    end
+    
+    describe "when command proc and method passed" do
+      subject    { Ing::Command.new dummy_command_proc, meth, *args }
+      let(:meth) { "dummy" }
+      let(:args) { ["one", "two"] }
+      
+      it "should have command_meth == passed method" do
+        assert_equal meth.to_sym, subject.command_meth
+      end
+      
+      it "should have options == {}" do
+        assert_equal Hash.new, subject.options
+      end
+      
+      it "should have args == remaining args" do
+        assert_equal args, subject.args
+      end       
+    end
+    
+    describe "when command class and non-public-instance-method arg passed" do
+      subject    { Ing::Command.new dummy_command_class, arg, *args }
+      let(:arg)  { "remove_instance_variable" }
+      let(:args) { ["one", "two"] }
+      
+      it "should have command_meth == :call" do
+        assert_equal :call, subject.command_meth
+      end
+      
+      it "should have options == {}" do
+        assert_equal Hash.new, subject.options
+      end
+      
+      it "should have args == all passed args" do
+        assert_equal [arg] + args, subject.args
+      end      
+    end
+    
+    describe "when command proc and non-public-method arg passed" do
+      subject    { Ing::Command.new dummy_command_proc, arg, *args }
+      let(:arg)  { "extended" }
+      let(:args) { ["one", "two"] }
+      
+      it "should have command_meth == :call" do
+        assert_equal :call, subject.command_meth
+      end
+      
+      it "should have options == {}" do
+        assert_equal Hash.new, subject.options
+      end
+      
+      it "should have args == all passed args" do
+        assert_equal [arg] + args, subject.args
+      end          
+    end
+    
+    describe "when command class and option arg passed" do
+      subject    { Ing::Command.new dummy_command_class, arg, *args }
+      let(:arg)  { "--nonsense" }
+      let(:args) { ["one", "two"] }
+      
+      it "should have command_meth == :call" do
+        assert_equal :call, subject.command_meth
+      end
+      
+      it "should have options == {}" do
+        assert_equal Hash.new, subject.options
+      end
+      
+      it "should have args == all passed args" do
+        assert_equal [arg] + args, subject.args
+      end      
+    end
+    
+    describe "when command proc and option arg passed" do
+      subject    { Ing::Command.new dummy_command_proc, arg, *args }
+      let(:arg)  { "--help" }
+      let(:args) { ["one", "two"] }
+      
+      it "should have command_meth == :call" do
+        assert_equal :call, subject.command_meth
+      end
+      
+      it "should have options == {}" do
+        assert_equal Hash.new, subject.options
+      end
+      
+      it "should have args == all passed args" do
+        assert_equal [arg] + args, subject.args
+      end          
+    end
+    
+  end
+  
+  #-----------------------------------------------------------------------------
+  describe "#instance" do
+    subject             { Ing::Command.new command_class }
+    let(:command_class) { dummy_command_class }
+    
+    it "should return an instance of the passed class" do
+      assert_kind_of command_class, subject.instance
+    end
+    
+    describe "and passed class defines specify_options" do
+      
+      def mock_parser_given(args, ret={})
+        parser = MiniTest::Mock.new
+        parser.expect(:parser,nil)
+        parser.expect(:"parse!",ret,[args]) 
+        parser
+      end
+      
+      def expecting_parser_given(args, ret={})
+        Ing::Command.stub(:parser, mock_parser_given(args, ret)) do |stub|
+          #puts stub.parser.inspect
+          yield
+        end
+      end
+ 
+# These don't work because `parse_options!` is indivisible from `instance` 
+# and you need an unstubbed :command_class for `parse_options!`
+#
+#      def mock_command_constructor_given(opts)
+#        cmd = MiniTest::Mock.new
+#        cmd.expect(:new,nil,[opts])
+#        cmd
+#      end
+#      
+#      def expecting_command_constructor_given(ing_cmd, opts)
+#        ing_cmd.stub(:command_class, 
+#                     mock_command_constructor_given(opts)) do |stub|
+#          #puts stub.command_class.inspect
+#          yield
+#        end
+#      end
+      
+      subject { Ing::Command.new command_class, *args }
+      let(:command_class) { dummy_command_class_with_specify_options }
+      let(:args)          { ["one", "two", "three"] }
+      
+      it "should interact with parser as expected" do
+        expecting_parser_given(args) do
+          subject.instance
+        end
+      end
+      
+      describe "and an option hash is passed as the last arg" do
+        subject              { Ing::Command.new command_class, *args, options }
+        let(:command_class)  { dummy_command_class_with_specify_options }
+        let(:args)           { ["--two", "2", "--four", "4", "--three", "1"] }
+        let(:options)        { {:one => 1, :two => 2, :three => 3} }
+        let(:parsed_options) { {:two => 2, :four => 4, :three => 1} }
+        
+        it "should merge passed options with the options parsed from other args" do
+          expecting_parser_given(args, parsed_options) do
+            merged_options = options.merge(parsed_options)
+            it = subject.instance
+            # note dummy.options returns what it was passed in constructor
+            assert_equal merged_options, it.options   
+          end
+        end
+   
+      end
+      
+    end
+    
+  end
+  
+  #-----------------------------------------------------------------------------
+  describe "#execute" do
+  
+    def mock_instance_sent(meth, args=[])
+      inst = MiniTest::Mock.new
+      inst.expect(:send, nil, [meth] + args)
+      inst
+    end
+    
+    def expecting_instance_sent(ing_cmd, meth, args=[])
+      ing_cmd.stub(:instance, mock_instance_sent(meth, args)) do |stub|
+        #puts stub.instance.inspect
+        yield
+      end
+    end
+    
+    describe "when only command class passed" do
+      subject { Ing::Command.new dummy_command_class }
+      
+      it "instance should receive :call and no args" do
+        subject.instance
+        expecting_instance_sent(subject, :call) do
+          subject.execute
+        end
+      end
+      
+    end
+
+    describe "when command class and options hash passed" do
+      subject       { Ing::Command.new dummy_command_class, options }
+      let(:options) { {:one => 1, :two => 2} }
+    
+      it "instance should receive :call and no args" do
+        subject.instance
+        expecting_instance_sent(subject, :call) do
+          subject.execute
+        end
+      end
+      
+    end
+    
+    describe "when command class and method passed with args" do
+      subject    { Ing::Command.new dummy_command_class, meth, *args }
+      let(:meth) { "dummy" }
+      let(:args) { ["one", "two"] }
+
+      it "instance should receive passed method and args" do
+        subject.instance
+        expecting_instance_sent(subject, meth.to_sym, args) do
+          subject.execute
+        end
+      end
+            
+    end
+    
+    describe "when command proc and method passed" do
+      subject    { Ing::Command.new dummy_command_proc, meth, *args }
+      let(:meth) { "dummy" }
+      let(:args) { ["one", "two"] }
+      
+      it "instance should receive passed method, args, and empty options hash" do
+        subject.instance
+        expecting_instance_sent(subject, meth.to_sym, args + [{}]) do
+          subject.execute
+        end
+      end
+      
+    end
+
+    describe "when command proc and options hash passed" do
+      subject       { Ing::Command.new dummy_command_proc, options }
+      let(:options) { {:one => 1, :two => 2} }
+      
+      it "instance should receive :call and passed options hash" do
+        subject.instance
+        expecting_instance_sent(subject, :call, [options]) do
+          subject.execute
+        end
+      end
+      
+    end
+    
+  end
+
+end
