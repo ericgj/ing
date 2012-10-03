23c87dd47cab914ff34f54530e9af8a6df47cbc1
Eric Gjertsen <ericgj72@gmail.com>
2012-10-03 02:08:11 -0400

    fix bash completion for commas in args
    although not ideal since it changes the global COMP_WORDBREAKS

--- completions/ing.bash
+++ completions/ing.bash
@@ -1,16 +1,27 @@
 # ing autocompletion
 
-_ing_builtins=(list help generate setup implicit base)
+# hopefully this doesn't cause too much havoc elsewhere
+# but srsly, I hate how COMP_WORDS breaks on commas!
+#
+COMP_WORDBREAKS=${COMP_WORDBREAKS/:}
+export COMP_WORDBREAKS
 
-_ing_is_builtin() {
-  for e in ${_ing_builtins[@]}; do [[ "$e" == "$1" ]] && return 0; done; return 1;
+# This would be ideal & not require global changes, but for some reason doesn't
+# want to work with a trailing comma in $cur
+_ing0() {
+  COMPREPLY=()
+  local cur; local words
+  _get_comp_words_by_ref -n : cur words
+  #echo -n "{ ing completion "${words[@]:1}" -- "$cur" }"
+  COMPREPLY=( $(compgen -W "$(ing completion "${words[@]:1}")" -- "$cur") ) 
+  __ltrim_colon_completions "$cur"  
 }
 
 _ing() {
   COMPREPLY=()
-  local word="${COMP_WORDS[COMP_CWORD]}"
-
-  COMPREPLY=( $(compgen -W "$(ing completion ${COMP_WORDS[@]})" -- "$word") )  
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  #echo -n "{ ing completion "${COMP_WORDS[@]:1}" -- "$cur" }"
+  COMPREPLY=( $(compgen -W "$(ing completion "${COMP_WORDS[@]:1}")" -- "$cur") ) 
 }
 
 complete -F _ing ing
